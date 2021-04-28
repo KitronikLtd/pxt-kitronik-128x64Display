b81d021734d20442ed96304460ed2b4a01d303e7
Kitronik Ltd <design@kitronik.co.uk>
2021-04-28 12:17:05 +0100

    added tests

--- test.ts
+++ test.ts
@@ -1,1 +1,25 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let item = 0
+kitronik_VIEW128x64.init(60)
+kitronik_VIEW128x64.drawRect(
+60,
+30,
+0,
+0
+)
+kitronik_VIEW128x64.show(
+"Hello!",
+1
+)
+kitronik_VIEW128x64.show(
+"1234567890",
+2
+)
+item = 0
+basic.forever(() => {
+    kitronik_VIEW128x64.show(
+    item,
+    3
+    )
+    item += 1
+    basic.pause(1000)
+})
