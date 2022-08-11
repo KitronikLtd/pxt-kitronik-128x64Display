430924379ab848b7cd803b1271ed5ff7fe9c99c6
JackAtKitronik <jack.faulkner@kitronik.co.uk>
2022-08-11 14:18:18 +0100

    Multiply x before setting buffer

--- main.ts
+++ main.ts
@@ -462,14 +462,14 @@ namespace kitronik_VIEW128x64 {
 
             for (let charOfString = 0; charOfString < displayString.length; charOfString++) {
                 charDisplayBytes = font[displayString.charCodeAt(charOfString)]
-                for (let k = 0; k < 5; k++) {  // 'for' loop will take byte font array and load it into the correct register, then shift to the next byte to load into the next location
+                for (let k = 0; k < 10; k++) {  // 'for' loop will take byte font array and load it into the correct register, then shift to the next byte to load into the next location
                     col = 0
                     for (let l = 0; l < 5; l++) {
-                        if (charDisplayBytes & (1 << (5 * k + l)))
+                        if (charDisplayBytes & (1 << (10 * k + l)))
                             col |= (1 << (l + 1))
                     }
 
-                    ind = (x + charOfString) * 5 + y * 128 + k + 1
+                    ind = (x + charOfString) * 10 + y * 128 + k + 1
                     screenBuf[ind] = col
                 }
             }
