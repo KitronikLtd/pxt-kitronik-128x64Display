29ad99687c7b013f6788c3bccd07e63b1557570f
JackAtKitronik <jack.faulkner@kitronik.co.uk>
2022-08-11 13:52:56 +0100

    Bit manipulation on font bytes

--- main.ts
+++ main.ts
@@ -462,6 +462,7 @@ namespace kitronik_VIEW128x64 {
 
             for (let charOfString = 0; charOfString < displayString.length; charOfString++) {
                 charDisplayBytes = font[displayString.charCodeAt(charOfString)]
+                charDisplayBytes = charDisplayBytes << 1
                 for (let k = 0; k < 5; k++) {  // 'for' loop will take byte font array and load it into the correct register, then shift to the next byte to load into the next location
                     col = 0
                     for (let l = 0; l < 5; l++) {
