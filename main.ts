267a748602bfaa7198bbd675dd73d6e34ee1c8c1
JackAtKitronik <jack.faulkner@kitronik.co.uk>
2022-08-12 12:14:41 +0100

    Trying to alter screenBuf loop for more height

--- main.ts
+++ main.ts
@@ -441,7 +441,7 @@ namespace kitronik_VIEW128x64 {
                 for (let k = 0; k < 5; k++) {  // 'for' loop will take byte font array and load it into the correct register, then shift to the next byte to load into the next location
                     col = 0
                     for (let l = 0; l < 5; l++) {
-                        if (charDisplayBytes & (1 << (5 * k + l)))
+                        if (charDisplayBytes & (1 << (5 * k + l * fontZoom)))
                             col |= (1 << (l * fontZoom + 1))
                     }
 
